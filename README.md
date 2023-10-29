ff7d3e68cc8a5ba89ba772efb74c620973ccfe8b
Pavel Kutsenko <zebartcoc@gmail.com>
2023-10-29 08:48:41 +0300

    Initial commit. Base logic for package.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Django Simple Components
+
+Django Simple Components is a small package to easily create components inside your templates without saving them in the templates folder.
+
+## Quick start
+
+1. Add `simple_components` to your INSTALLED_APPS setting like this:
+```python
+INSTALLED_APPS = [
+    ...,
+    "simple_components",
+]
+```
+
+2. Add `simple_components` to your template like this:
+```html
+{% load simple_components %}
+
+{% set_component "first_component" %}
+    <div class="card">
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+    </div>
+{% end_set_component %}
+
+<div class="card-list">
+    {% component "first_component" title="Hello world!" description="Some text..." %}
+    {% component "first_component"
+        title="Some lines"
+        description="Other text..."
+    %}
+
+    {% with value="this text will be capitalized later" %}
+        {% component "first_component" title=123 description=value|capfirst %}
+    {% endwith %}
+</div>
+```
+
+3. Hooray! Everything is ready to use it.
+
+## Contributing
+If you would like to suggest a new feature, you can create an issue on the GitHub repository for this project. 
+If you'd like to contribute code, you can fork the repository and submit a pull request with your changes.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for more information.
---
+++ simple_components/templatetags/simple_components.py
@@ -0,0 +1,86 @@
+import re
+from django import template
+
+
+# allow line breaks inside tags
+template.base.tag_re = re.compile(template.base.tag_re.pattern, re.DOTALL)
+
+
+register = template.Library()
+
+
+@register.tag
+def set_component(parser, token):
+    nodelist = parser.parse(("end_set_component",))
+    parser.delete_first_token()
+
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise template.TemplateSyntaxError(
+            "'%s' takes at least one argument (name of component)" % bits[0]
+        )
+
+    component_name = str(parser.compile_filter(bits[1]))
+
+    return SetComponentNode(component_name, nodelist)
+
+
+class SetComponentNode(template.Node):
+    def __init__(self, component_name, nodelist):
+        self.component_name = component_name
+        self.nodelist = nodelist
+
+    def render(self, context):
+        if 'components' not in context:
+            context['components'] = {}
+        context['components'][self.component_name] = self.nodelist
+
+        return ''
+
+
+@register.tag
+def component(parser, token):
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise template.TemplateSyntaxError(
+            '\'%s\' takes at least one argument (name of component)' % bits[0]
+        )
+
+    component_name = str(parser.compile_filter(bits[1]))
+
+    kwargs = {}
+    bits = bits[2:]
+    for bit in bits:
+        match = re.compile(r'(?:(\w+)=)?(.+)').match(bit)
+
+        name, value = match.groups()
+        if name:
+            kwargs[name] = parser.compile_filter(value)
+        else:
+            raise template.TemplateSyntaxError(
+                '\'%s\' must be takes as kwargs' % bit
+            )
+
+    return ComponentNode(component_name, kwargs)
+
+
+class ComponentNode(template.Node):
+    def __init__(self, component_name=None, kwargs=None):
+        if component_name is None:
+            raise template.TemplateSyntaxError(
+                "Component template nodes must be given a name to return."
+            )
+
+        self.component_name = component_name
+
+        if kwargs is None:
+            kwargs = {}
+
+        self.kwargs = kwargs
+
+    def render(self, context):
+        nodelist = context['components'][self.component_name]
+
+        for key, value in self.kwargs.items():
+            context[key] = value.resolve(context)
+        return nodelist.render(context)
